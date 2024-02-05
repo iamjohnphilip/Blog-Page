26a26c1beb8af360848ca9a33653024effd77edb
iamjohnphilip <146698412+iamjohnphilip@users.noreply.github.com>
2024-02-05 17:44:36 +0800

    Add files via upload

---
+++ blog.php
@@ -0,0 +1,93 @@
+<?php
+session_start();
+
+?>
+
+
+
+
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE-edge">
+    <meta name="viewport" content="width=device-width, intial-scale=1.0, maximum-scale=1.0, user-scalable=no">
+    <title>Blog Section</title>
+    <link rel="stylesheet" type="text/css" href="blog.css"/>
+    <link rel="shortcut icon" href="images/fav-icon.svg"/>
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
+    </head>
+    <body>
+        <section id="blog">
+            <div class="blog-heading">
+              <span>John Philip Raman</span>
+              <h3>Blog</h3>
+            </div>
+      
+      
+            <div class="blog-container">
+      
+              <div class="blog-box">
+      
+
+                <div class="blog-img">
+                  <img alt="blog" src="picture/MJ.jfif">
+                </div>
+
+                <div class="blog-text">
+                  <span>This is Michael Jordan.</span>
+                  <a href="#" class="blog-title">His Airness</a>
+                  <p>During his playing career, Michael Jordan, a guard, was an exceptionally talented shooter and passer and a tenacious defender. He earned the nickname “Air Jordan” because of his extraordinary leaping ability and acrobatic maneuvers, and his popularity reached heights few athletes have known.</p>
+                  <a href="faker.html">Read More</a>
+                </div>
+      
+              </div>
+      
+      
+      
+      
+   
+              <div class="blog-box">
+      
+                <div class="blog-img">
+                  <img alt="blog" src="picture/DB.jfif">
+                </div>
+                <div class="blog-text">
+                  <span>My First Anime</span>
+                  <a href="#" class="blog-title">Dragon Ball</a>
+                  <p>Dragon Ball tells the tale of a young warrior by the name of Son Goku, a young peculiar boy with a tail who embarks on a quest to become stronger and learns of the Dragon Balls, when, once all 7 are gathered, grant any wish of choice.</p>
+                  <a href="faker.html">Read More</a>
+                </div>
+      
+              </div>
+      
+      
+      
+      
+              
+              <div class="blog-box">
+      
+                
+                <div class="blog-img">
+                  <img alt="blog" src="picture/Kwek-Kwek.jfif">
+                </div>
+                
+                <div class="blog-text">
+                  <span>Popular Street Food in my Country</span>
+                  <a href="#" class="blog-title">Kwek-Kwek</a>
+                  <p>A popular street food in the Philippines, kwek kwek are hard-boiled quail eggs in orange batter that are deep fried and served with a sweet and sour sauce.</p>
+                  <a href="faker.html">Read More</a>
+                </div>
+      
+              </div>
+      
+            </div>
+      
+          </section>
+          
+    </body>
+    </html>
